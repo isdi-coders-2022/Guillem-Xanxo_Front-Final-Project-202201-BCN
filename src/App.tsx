71162d3aa2c1fb8a7d53888c43493f3196985f1c
GuillemXanxo <jscodebaker@gmail.com>
2022-03-08 09:53:31 +0100

    Added Router in src/index and App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes, Link } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return;
+  <>
+    <Routes>
+      <Route path="/*" element={<TripsCronoPage />}></Route>
+      <Route path="/viatges/crono" element={<TripsCronoPage />}></Route>
+    </Routes>
+    ;
+  </>;
 }
 
 export default App;
