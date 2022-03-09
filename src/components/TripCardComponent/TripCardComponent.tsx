bdb14590ee28cbcbe07999b291a132b96e2c38e4
GuillemXanxo <jscodebaker@gmail.com>
2022-03-09 14:11:47 +0100

    Created Card for trip with conditional rendering

--- src/components/TripCardComponent/TripCardComponent.tsx
+++ src/components/TripCardComponent/TripCardComponent.tsx
@@ -1,1 +1,34 @@
-export {};
+import { useLocation } from "react-router-dom";
+import { Trip } from "../../interfaces/Trip";
+interface TripCardProps {
+  trip: Trip;
+}
+export const TripCard = ({
+  trip: { origen, desti, places, data, horaSortida, comentaris, dones, id },
+}: TripCardProps): JSX.Element => {
+  const location = useLocation();
+
+  return (
+    <>
+      <p className="categoria">Origen:</p>
+      <p className="categoria_negreta">{origen}</p>
+      <p className="categoria">Destí:</p>
+      <p className="categoria_negreta">{desti}</p>
+      <p className="categoria">Places:</p>
+      <p className="categoria_negreta">{places}</p>
+      <p className="categoria">Hora sortida::</p>
+      <p className="categoria_negreta">{horaSortida}</p>
+      {location.pathname === id && (
+        <>
+          <p className="categoria_negreta">Comentaris:</p>
+          <p className="categoria">{comentaris}</p>
+        </>
+      )}
+      {dones === true && (
+        <p className="categoria_negreta">
+          Aquest viatge és exclusiu per a passetgeres
+        </p>
+      )}
+    </>
+  );
+};
