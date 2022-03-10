2f2f492fdf89e64bc487f449883b06b70b2eaad7
GuillemXanxo <jscodebaker@gmail.com>
2022-03-10 16:08:37 +0100

    Added test for App and accomplished first Read

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,8 @@ import styled from "styled-components";
 import ShowTripsPage from "./pages/ShowTripsPage";
 
 function App() {
-  const AppAppearence = styled.div`
-    background-color: #ebe7e7;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  `;
-
   return (
     <>
-      <AppAppearence>
-        <ShowTripsPage></ShowTripsPage>
-      </AppAppearence>
       <Routes>
         <Route path="/*" element={<ShowTripsPage />}></Route>
         <Route path="/viatges/crono" element={<ShowTripsPage />}></Route>
--- src/pages/ShowTripsPage.tsx
+++ src/pages/ShowTripsPage.tsx
@@ -1,10 +1,22 @@
 import HeaderComponent from "../components/HeaderComponent/HeaderComponent";
-// import TripCatwalkComponent from "../components/TripCatwalkComponent/TripCatwalkComponent";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { RootState } from "../redux/store";
+import { getAllTripsThunk } from "../redux/thunks/tripsThunks";
+import TripCatwalkComponent from "../components/TripCatwalkComponent/TripCatwalkComponent";
 
 const ShowTripsPage = (): JSX.Element => {
+  const dispatch = useDispatch();
+  const tripList = useSelector((state: RootState) => state.trips);
+
+  useEffect(() => {
+    dispatch(getAllTripsThunk);
+  }, [dispatch]);
+
   return (
     <>
       <HeaderComponent />
+      <TripCatwalkComponent trips={tripList} />
     </>
   );
 };
