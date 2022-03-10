6cf05e63e5603bbc5ee42f53006a4f490436b975
GuillemXanxo <jscodebaker@gmail.com>
2022-03-10 18:32:21 +0100

    Added styles to general layout so it shows cards

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,5 +1,6 @@
 import Button from "../ButtonComponent/ButtonComponent";
 import { useNavigate } from "react-router-dom";
+import { HeaderComponentStyles } from "./HeaderComponent.styles";
 
 const HeaderComponent = (): JSX.Element => {
   const navigate = useNavigate();
@@ -10,13 +11,15 @@ const HeaderComponent = (): JSX.Element => {
 
   return (
     <>
-      <h1 className="title">Transporta't Pallars</h1>
-      <Button
-        className="button-usuari"
-        type="usuari"
-        text=""
-        actionOnClick={goToHome}
-      />
+      <HeaderComponentStyles>
+        <h1 className="title">Transporta't Pallars</h1>
+        <Button
+          className="button-usuari"
+          type="usuari"
+          text=""
+          actionOnClick={goToHome}
+        />
+      </HeaderComponentStyles>
     </>
   );
 };
---
+++ src/components/TripCardComponent/TripCardComponent.styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const TripCardComponentStyles = styled.header`
+  padding: 25px;
+  background-color: #e5e5e5;
+  margin: 20px;
+`;
