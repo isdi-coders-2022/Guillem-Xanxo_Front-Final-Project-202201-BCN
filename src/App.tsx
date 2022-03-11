0f01cadcd6c4cc6cbb13fa3ca75c92b6cf8fb855
GuillemXanxo <jscodebaker@gmail.com>
2022-03-11 10:48:06 +0100

    Deleted ; that was in App

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ function App() {
         <Route path="/*" element={<ShowTripsPage />}></Route>
         <Route path="/viatges/crono" element={<ShowTripsPage />}></Route>
       </Routes>
-      ;
     </>
   );
 }
