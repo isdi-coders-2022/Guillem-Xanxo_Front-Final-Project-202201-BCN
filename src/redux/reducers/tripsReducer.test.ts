2d5f89fe0711a2059e36cd4e161967061f6be052
GuillemXanxo <jscodebaker@gmail.com>
2022-03-14 11:31:48 +0100

    Added test for create reducer

--- src/redux/reducers/tripsReducer.test.ts
+++ src/redux/reducers/tripsReducer.test.ts
@@ -66,7 +66,7 @@ describe("Given a tripsReducer function", () => {
   });
 
   describe("When it is called with a deleteThisTripAction action", () => {
-    test.only("Then it should return an array of trips without the trip with id passed as argument", () => {
+    test("Then it should return an array of trips without the trip with id passed as argument", () => {
       const currentTrips: Trip[] = [
         {
           origen: "Barcelona",
@@ -103,6 +103,43 @@ describe("Given a tripsReducer function", () => {
           dones: false,
           data: "2018-02-12 19:23:45",
           id: "2",
+        },
+      ];
+
+      const newState = tripsReducer(currentTrips, action);
+
+      expect(newState).toEqual(expectedTripArray);
+    });
+  });
+
+  describe("When it is called with a createThisTripAction action", () => {
+    test.only("Then it should return an array of trips with the trip passed as argument", () => {
+      const currentTrips: Trip[] = [];
+      const newTrip = {
+        origen: "Barcelona",
+        desti: "Sort",
+        places: 3,
+        horaSortida: 18,
+        comentaris: "S'accepten animals",
+        dones: false,
+        data: "2018-02-12 19:23:45",
+        id: "1",
+      };
+
+      const action = {
+        type: "create-trip",
+        newTrip: newTrip,
+      };
+      const expectedTripArray: Trip[] = [
+        {
+          origen: "Barcelona",
+          desti: "Sort",
+          places: 3,
+          horaSortida: 18,
+          comentaris: "S'accepten animals",
+          dones: false,
+          data: "2018-02-12 19:23:45",
+          id: "1",
         },
       ];
 
