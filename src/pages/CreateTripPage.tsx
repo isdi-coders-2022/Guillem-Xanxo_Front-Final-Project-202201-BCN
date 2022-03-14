3924405b1812e957b60c3ea6d90fa58f79eff2ba
GuillemXanxo <jscodebaker@gmail.com>
2022-03-14 17:45:53 +0100

    Created page to receive create form component

---
+++ src/pages/CreateTripPage.tsx
@@ -0,0 +1,12 @@
+import HeaderComponent from "../components/HeaderComponent/HeaderComponent";
+
+const createTripPage = () => {
+  return (
+    <>
+      <HeaderComponent />
+      <CreateTripComponent />
+    </>
+  );
+};
+
+export default createTripPage;
