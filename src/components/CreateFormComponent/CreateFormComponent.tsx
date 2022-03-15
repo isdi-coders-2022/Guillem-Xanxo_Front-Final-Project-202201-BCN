e7bafaf1c96ad6e02405de3b1e728e92bee4f4a0
GuillemXanxo <jscodebaker@gmail.com>
2022-03-15 09:19:37 +0100

    Deleted blankfields to pass audit

--- src/components/CreateFormComponent/CreateFormComponent.tsx
+++ src/components/CreateFormComponent/CreateFormComponent.tsx
@@ -1,14 +1,4 @@
 const CreateFormComponent = () => {
-  const blankFields = {
-    origen: "",
-    desti: "",
-    data: "",
-    places: "",
-    horaSortida: "",
-    comentaris: "",
-    dones: "",
-  };
-
   return (
     <>
       <h1>HOLA</h1>
