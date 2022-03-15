fd6634cfa30766caef9df7646e88a433da0b6403
GuillemXanxo <jscodebaker@gmail.com>
2022-03-15 10:51:21 +0100

    Added all functionality to form component

--- src/components/CreateFormComponent/CreateFormComponent.tsx
+++ src/components/CreateFormComponent/CreateFormComponent.tsx
@@ -1,24 +1,33 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Trip } from "../../interfaces/Trip";
+import { createTripThunk } from "../../redux/thunks/tripsThunks";
 import Button from "../ButtonComponent/ButtonComponent";
 
-const CreateFormComponent = () => {
+const CreateFormComponent = (): JSX.Element => {
+  const dispatch = useDispatch();
+
   const blankFields = {
     origen: "",
     desti: "",
     data: "",
-    places: "",
-    horaSortida: "",
+    places: 0,
+    horaSortida: 0,
     comentaris: "",
-    dones: "",
+    dones: false,
+    id: "0",
   };
+
+  const [formData, setFormData] = useState<Trip>(blankFields);
 
   const isFilled =
     formData.origen !== "" &&
     formData.desti !== "" &&
     formData.data !== "" &&
-    formData.places !== "" &&
-    formData.horaSortida !== "";
+    formData.places !== 0 &&
+    formData.horaSortida !== 0;
 
-  const changeData = (event) => {
+  const changeData = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setFormData({
       ...formData,
       [event.target.id]:
@@ -32,64 +41,83 @@ const CreateFormComponent = () => {
     setFormData(blankFields);
   };
 
+  const onFormSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    formData.id = Date.now.toString();
+    dispatch(createTripThunk(formData));
+    resetForm();
+  };
+
   return (
     <>
-      <div className="form-group">
-        <label htmlFor="origen">Origen: </label>
-        <input
-          type="text"
-          id="origen"
-          className="form-input"
-          value={formData.origen}
-          onChange={changeData}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="desti">Destí: </label>
-        <input
-          type="text"
-          id="desti"
-          className="form-input"
-          value={formData.desti}
-          onChange={changeData}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="places">Places: </label>
-        <input
-          type="number"
-          id="places"
-          className="form-input"
-          value={formData.places}
-          onChange={changeData}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="horaSortida">Hora de sortida: </label>
-        <input
-          type="time"
-          id="horaSortida"
-          className="form-input"
-          value={formData.horaSortida}
-          onChange={changeData}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="comentaris">Comentaris: </label>
-        <input
-          type="text"
-          id="comentaris"
-          className="form-input"
-          value={formData.comentaris}
-          onChange={changeData}
-        />
-      </div>
-      <Button
-        type="submit"
-        disabled={!isFilled}
-        src={"Confirm"}
-        alt="Crea el viatge"
-      ></Button>
+      <form onSubmit={onFormSubmit} autoComplete="off">
+        <div className="form-group">
+          <label htmlFor="origen">Origen: </label>
+          <input
+            type="text"
+            id="origen"
+            className="form-input"
+            value={formData.origen}
+            onChange={changeData}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="desti">Destí: </label>
+          <input
+            type="text"
+            id="desti"
+            className="form-input"
+            value={formData.desti}
+            onChange={changeData}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="places">Places: </label>
+          <input
+            type="number"
+            id="places"
+            className="form-input"
+            value={formData.places}
+            onChange={changeData}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="horaSortida">Hora de sortida: </label>
+          <input
+            type="time"
+            id="horaSortida"
+            className="form-input"
+            value={formData.horaSortida}
+            onChange={changeData}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="data">Data: </label>
+          <input
+            type="date"
+            id="data"
+            className="form-input"
+            value={formData.data}
+            onChange={changeData}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="comentaris">Comentaris: </label>
+          <input
+            type="text"
+            id="comentaris"
+            className="form-input"
+            value={formData.comentaris}
+            onChange={changeData}
+          />
+        </div>
+        <Button
+          type="submit"
+          disabled={!isFilled}
+          src={"Confirm"}
+          alt="Crea el viatge"
+        ></Button>
+      </form>
     </>
   );
 };
