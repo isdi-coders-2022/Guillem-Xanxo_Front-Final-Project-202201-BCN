82b8dfcb2abfd6d5a6404aff81e0fb1135ed640a
GuillemXanxo <jscodebaker@gmail.com>
2022-03-15 09:17:19 +0100

    Added blankfields to the component form

--- src/components/CreateFormComponent/CreateFormComponent.tsx
+++ src/components/CreateFormComponent/CreateFormComponent.tsx
@@ -1,4 +1,14 @@
 const CreateFormComponent = () => {
+  const blankFields = {
+    origen: "",
+    desti: "",
+    data: "",
+    places: "",
+    horaSortida: "",
+    comentaris: "",
+    dones: "",
+  };
+
   return (
     <>
       <h1>HOLA</h1>
