9174fe8eba9e8e849a0e94ad625b0ea173a80c9b
GuillemXanxo <jscodebaker@gmail.com>
2022-03-17 12:19:08 +0100

    Created page to register

---
+++ src/pages/UserRegisterPage.tsx
@@ -0,0 +1,12 @@
+import RegisterFormComponent from "../components/RegisterFormComponent/RegisterFormComponent";
+
+const UserRegisterPage = () => {
+  return (
+    <>
+      <h1 className="title_login">Transporta't Pallars</h1>
+      <RegisterFormComponent />
+    </>
+  );
+};
+
+export default UserRegisterPage;
