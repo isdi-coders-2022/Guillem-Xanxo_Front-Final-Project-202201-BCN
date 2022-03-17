e4abf0f99d8d2779f4f101dd0779d5adcc45b570
GuillemXanxo <jscodebaker@gmail.com>
2022-03-17 15:47:47 +0100

    Added postman json

--- src/pages/UserLoginPage.tsx
+++ src/pages/UserLoginPage.tsx
@@ -1,16 +1,9 @@
 import LoginComponent from "../components/LoginComponent/LoginComponent";
-import styled from "styled-components";
-
-const mapBackground = styled.form`
-  background-image: url("src/assets/map.webp");
-`;
 
 const UserLoginPage = () => {
   return (
     <>
-      <mapBackground>
-        <LoginComponent />
-      </mapBackground>
+      <LoginComponent />
     </>
   );
 };
