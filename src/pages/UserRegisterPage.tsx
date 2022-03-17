1a370f107bf602f950720673b16a49bed147aac4
GuillemXanxo <jscodebaker@gmail.com>
2022-03-17 16:14:54 +0100

    Background image to register page

--- src/pages/UserRegisterPage.tsx
+++ src/pages/UserRegisterPage.tsx
@@ -1,9 +1,19 @@
 import RegisterFormComponent from "../components/RegisterFormComponent/RegisterFormComponent";
+import styled from "styled-components";
+import img from "../assets/map.webp";
+
+const Content = styled.div`
+  background-image: url(${img});
+  width: 100%;
+  height: 100vh;
+`;
 
 const UserRegisterPage = () => {
   return (
     <>
-      <RegisterFormComponent />
+      <Content>
+        <RegisterFormComponent />
+      </Content>
     </>
   );
 };
