51ebf311d1597ff0ff4147e7ad38c559210a0d14
GuillemXanxo <jscodebaker@gmail.com>
2022-03-17 16:23:55 +0100

    Creted test for Register form component

---
+++ src/components/RegisterFormComponent/RegisterFormComponent.test.tsx
@@ -0,0 +1,59 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import { store } from "../../redux/store";
+import RegisterFormComponent from "./RegisterFormComponent";
+
+const mockUseDispatch = jest.fn();
+
+jest.mock("react-redux", () => {
+  return {
+    ...jest.requireActual("react-redux"),
+    useDispatch: () => mockUseDispatch,
+  };
+});
+
+describe("Given a Register component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should display 3 text boxes", () => {
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <RegisterFormComponent />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const text = screen.getAllByRole("textbox");
+
+      expect(text).toHaveLength(3);
+    });
+  });
+
+  describe("When it is filled with the required fields", () => {
+    test("Then it should display the submit button", () => {
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <RegisterFormComponent />
+          </Provider>
+        </BrowserRouter>
+      );
+      userEvent.type(screen.getByLabelText("Usuari:"), "Luis");
+      userEvent.type(screen.getByLabelText("Contrassenya:"), "122112");
+      userEvent.type(
+        screen.getByLabelText("Telèfon: (públic quan es mostren els viatges)"),
+        "611111111"
+      );
+      userEvent.type(
+        screen.getByLabelText("Nom: (públic quan es mostren els viatges)"),
+        "Luis"
+      );
+
+      const button = screen.getByRole("button", { name: /Registra't/i });
+
+      expect(button).toBeInTheDocument();
+    });
+  });
+});
