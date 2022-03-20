4c8105e861f3eebef6e9e413549993261ffca8ef
GuillemXanxo <jscodebaker@gmail.com>
2022-03-20 12:21:30 +0100

    Added test for oneTrip reducer

---
+++ src/redux/reducers/oneTripReducer.test.ts
@@ -0,0 +1,43 @@
+import { TripReceived } from "../../interfaces/TripReceived";
+import actionTypes from "../actions/actionTypes";
+import OneTripReducer from "./OneTripReducer";
+
+describe("Given a OneTripReducer function", () => {
+  describe("When it's called with a getThisTrip action with an object like TripReceived", () => {
+    test("Then it should return a new state with the trip received", () => {
+      const currentTrip: TripReceived = {
+        origen: "",
+        desti: "",
+        places: "",
+        data: "",
+        dataNumber: 0,
+        horaSortida: "",
+        horaSortidaNumber: 0,
+        comentaris: "",
+        dones: false,
+        id: "",
+      };
+      const tripToGet: TripReceived = {
+        origen: "Barcelona",
+        desti: "Sort",
+        places: "3",
+        horaSortida: "18",
+        horaSortidaNumber: 18,
+        comentaris: "S'accepten animals",
+        dones: false,
+        data: "2018-02-12",
+        dataNumber: 20180212,
+        id: "1",
+      };
+
+      const action = {
+        type: actionTypes.getThisTrip,
+        trip: tripToGet,
+      };
+
+      const newState = OneTripReducer(currentTrip, action);
+
+      expect(newState).toEqual(tripToGet);
+    });
+  });
+});
