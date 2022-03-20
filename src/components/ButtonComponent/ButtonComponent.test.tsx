666eca4c1fded3eb6f4217e86ba5e3b71cea28d0
GuillemXanxo <jscodebaker@gmail.com>
2022-03-20 11:17:51 +0100

    Added tests for button with action

--- src/components/ButtonComponent/ButtonComponent.test.tsx
+++ src/components/ButtonComponent/ButtonComponent.test.tsx
@@ -59,4 +59,80 @@ describe("Given a Button component", () => {
       expect(action).toHaveBeenCalled();
     });
   });
+
+  describe("When it receives an action and a button with type home is clicked", () => {
+    test("Then it should call the action", () => {
+      const action = jest.fn();
+
+      render(
+        <Button
+          actionOnClick={action}
+          text="text"
+          nature="home"
+          className="button"
+        />
+      );
+
+      userEvent.click(screen.getByRole("button"));
+
+      expect(action).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives an action and a button with type cerca-viatge is clicked", () => {
+    test("Then it should call the action", () => {
+      const action = jest.fn();
+
+      render(
+        <Button
+          actionOnClick={action}
+          text="text"
+          nature="cerca-viatge"
+          className="button"
+        />
+      );
+
+      userEvent.click(screen.getByRole("button"));
+
+      expect(action).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives an action and a button with type detall is clicked", () => {
+    test("Then it should call the action", () => {
+      const action = jest.fn();
+
+      render(
+        <Button
+          actionOnClick={action}
+          text="text"
+          nature="detall"
+          className="button"
+        />
+      );
+
+      userEvent.click(screen.getByRole("button"));
+
+      expect(action).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives an action and a button with type enrere-crono is clicked", () => {
+    test("Then it should call the action", () => {
+      const action = jest.fn();
+
+      render(
+        <Button
+          actionOnClick={action}
+          text="text"
+          nature="enrere-crono"
+          className="button"
+        />
+      );
+
+      userEvent.click(screen.getByRole("button"));
+
+      expect(action).toHaveBeenCalled();
+    });
+  });
 });
