e2c265db68d5bd237d85e2be133de687f8d5d36e
GuillemXanxo <jscodebaker@gmail.com>
2022-03-23 10:20:18 +0100

    Added test for user home page buttons

--- src/pages/UserHomePage.test.tsx
+++ src/pages/UserHomePage.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "../redux/store";
 import UserHomePage from "./UserHomePage";
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
 
 describe("Given an UserHomePage page", () => {
   describe("When it's rendered", () => {
@@ -18,6 +26,57 @@ describe("Given an UserHomePage page", () => {
       const cercaViatge = screen.getByRole("button", { name: "Cerca viatge" });
 
       expect(cercaViatge).toBeInTheDocument();
+    });
+  });
+
+  describe("When the Crear viatge button is clicked", () => {
+    test("Then it should invoke a function", () => {
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <UserHomePage />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const deleteButton = screen.getByRole("button", { name: "Crear viatge" });
+      userEvent.click(deleteButton);
+
+      expect(mockNavigate).toHaveBeenCalled();
+    });
+  });
+
+  describe("When the Cerca viatge button is clicked", () => {
+    test("Then it should invoke a function", () => {
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <UserHomePage />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const deleteButton = screen.getByRole("button", { name: "Cerca viatge" });
+      userEvent.click(deleteButton);
+
+      expect(mockNavigate).toHaveBeenCalled();
+    });
+  });
+
+  describe("When the Tanca sessio button is clicked", () => {
+    test("Then it should invoke a function", () => {
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <UserHomePage />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const deleteButton = screen.getByRole("button", { name: "Tanca sessió" });
+      userEvent.click(deleteButton);
+
+      expect(mockNavigate).toHaveBeenCalled();
     });
   });
 });
